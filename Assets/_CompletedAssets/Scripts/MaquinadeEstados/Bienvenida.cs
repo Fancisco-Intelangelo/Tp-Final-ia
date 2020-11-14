ed41a224ecb0c726ec85eac6f0d58de3d18e4177
unknown <panchointelangelo1999@gmail.com>
2020-11-14 18:15:06 -0300

    pancho

---
+++ Assets/_CompletedAssets/Scripts/MaquinadeEstados/Bienvenida.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bienvenida : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy (gameObject, 3f);
+	}
+}
